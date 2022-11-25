721dae8547f72886e17174d7171bc1784e2a306d
shathipaul <shathipaul11@gmail.com>
2022-11-25 16:38:46 +0600

    added server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.send('The book spot server is running')
+})
+
+app.listen(port, () =>{
+    console.log(`The book spot server is running on ${port}`);
+})
