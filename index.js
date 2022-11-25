38b54bd7f1ec71f0eb94bb12bfb452062ca2f8c0
Shathi paul <108425880+shathipaul@users.noreply.github.com>
2022-11-26 03:47:07 +0600

    added try

--- index.js
+++ index.js
@@ -13,11 +13,44 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.j6khnv2.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+        const bookCategory = client.db('theBookSpot').collection('categoryName');
+        const bookCollection = client.db('theBookSpot').collection('categories');
+
+        app.get('/categories', async(req, res) =>{
+            const query ={}
+            const cursor = bookCategory.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        })
+        app.get('/category', async(req, res) =>{
+            const query ={}
+            const cursor = bookCollection.find(query);
+            const category = await cursor.toArray();
+            res.send(category);
+        })
+
+        // app.get('/category/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) }
+        //     const categoryId = await bookCollection.findOne(query);
+        //     res.send(categoryId);
+        // })
+        app.get('/categories/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id }
+            const cursor = bookCollection.find(query);
+            const categoryId = await cursor.toArray();
+            res.send(categoryId);
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(error =>console.error(error))
 
 
 app.get('/', (req, res) =>{
